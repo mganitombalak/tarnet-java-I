4f23211771a6c945dc37f230f85dc54cfe236b21
Mehmet Gani Tombalak <mgani.tombalak@yahoo.com>
2020-08-07 10:03:05 +0300

    Java Day-IV

--- src/com/tarnet/DbApplication.java
+++ src/com/tarnet/DbApplication.java
@@ -1,6 +1,5 @@
 package com.tarnet;
 
-import java.net.ConnectException;
 import java.sql.*;
 
 public class DbApplication {
@@ -12,17 +11,51 @@ public class DbApplication {
         try {
             Class.forName("org.h2.Driver");
             Connection conn = DriverManager.getConnection(connectionUrl, user, password);
-            Statement stmt= conn.createStatement();
-            ResultSet resultSet = stmt.executeQuery(query);
-            while(resultSet.next()){
-                System.out.printf("%d %s %n",resultSet.getInt(1),resultSet.getString(2));
-            }
+            Statement stmt = conn.createStatement();
+//            ResultSet resultSet = stmt.executeQuery(query); // Connected -> DataReader
+//            while(resultSet.next()){
+//                System.out.printf("%d %s %n",resultSet.getInt(1),resultSet.getString(2));
+//            }
+//            stmt.close();
+//            conn.close();
+
+//            RowSet rs = new CachedRowSetImpl();
+//            rs.setUrl(connectionUrl);
+//            rs.setCommand(query);
+//            rs.setUsername(user);
+//            rs.setPassword(password);
+//            rs.execute();
+//            while (rs.next()){
+//                System.out.printf("%d %s %n",rs.getInt(1),rs.getString(2));
+//            }
+//            rs.close();
+
+//            CachedRowSetImpl cachedRowSet = new CachedRowSetImpl();
+//            cachedRowSet.populate(resultSet);
+//            stmt.close();
+//            while (cachedRowSet.next()) {
+//                System.out.printf("%d %s %n", cachedRowSet.getInt(1), cachedRowSet.getString(2));
+//                if (cachedRowSet.getInt(1) == 2) {
+//                    cachedRowSet.updateString(2, "Edirne Tarim Koop");
+//                    cachedRowSet.updateRow();
+//                }
+//            }
+//            cachedRowSet.setTableName("customer");
+//            cachedRowSet.acceptChanges(conn);
+            conn.setAutoCommit(false);
+            String query1 = "UPDATE Customer set Name='Tekirdag Tarim Koop' Where id=2";
+            stmt.executeUpdate(query1);
+            Savepoint sp = conn.setSavepoint("TekirdagUpdated");
+            String query2 = "UPDATE Customer set Name='Antalya Tarim Koop' Where id=1";
+            stmt.executeUpdate(query2);
+            // FATAL ERROR
+            conn.rollback(sp);
+            conn.commit();
             stmt.close();
             conn.close();
-
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
-        } catch (SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
---
+++ src/com/tarnet/thread/ThreadApp.java
@@ -0,0 +1,9 @@
+package com.tarnet.thread;
+
+public class ThreadApp {
+    public static void main(String[] args) {
+        // Thread
+        // Runnable FireAndForget
+
+    }
+}
