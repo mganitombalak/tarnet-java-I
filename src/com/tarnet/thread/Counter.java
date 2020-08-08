6d2fcaa680305f36d9998c3c670f05d0dd806bfc
Mehmet Gani Tombalak <mgani.tombalak@yahoo.com>
2020-08-08 10:02:03 +0300

    Java Day-IV Threads

--- src/com/tarnet/thread/Counter.java
+++ src/com/tarnet/thread/Counter.java
@@ -2,33 +2,31 @@ package com.tarnet.thread;
 
 import lombok.Data;
 
-import java.util.concurrent.locks.ReentrantLock;
-
 @Data
-public class Counter implements Runnable {
+public class Counter {//implements Runnable {
 
-    private static ReentrantLock locker = new ReentrantLock();
+//    private static ReentrantLock locker = new ReentrantLock();
 
     private int value;
 
-    public void Inc() {
-        locker.lock();
-        try {
-            this.value++;
-        } finally {
-            locker.unlock();
-        }
+    public synchronized void Inc() {
+//        locker.lock();
+//        try {
+        this.value++;
+//        } finally {
+//            locker.unlock();
+//        }
     }
 
     public void Dec() {
         this.value--;
     }
 
-    @Override
-    public void run() {
-        while(this.value<1000){
-//            System.out.println("Thread:"+Thread.currentThread().getId() + " locked:"+locker.isLocked() + " isheld:"+ locker.isHeldByCurrentThread());
-            this.Inc();
-        }
-    }
+//    @Override
+//    public void run() {
+//        while(this.value<1000){
+////            System.out.println("Thread:"+Thread.currentThread().getId() + " locked:"+locker.isLocked() + " isheld:"+ locker.isHeldByCurrentThread());
+//            this.Inc();
+//        }
+//    }
 }
