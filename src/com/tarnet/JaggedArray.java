b85219570d6e2f3d9b1e898033ad6c4a21b3a312
yuznumaram <aturak@tarimkredi.org.tr>
2020-08-08 10:58:52 +0300

    hw02

--- src/com/tarnet/JaggedArray.java
+++ src/com/tarnet/JaggedArray.java
@@ -2,13 +2,19 @@ package com.tarnet;
 
 public class JaggedArray {
     public static void main(String[] args) {
-        int[][] array = new int[4][];
+        int[][] array = new int[7][9];
 
         for (int outer = 0; outer < array.length; outer++) {
-            array[outer] = new int[outer+1];
+            for (int x = 0; x < array[outer].length; x++) {
+                array[outer][x / 2] = 0;
+                if (array[outer][x] != 0)
+                    array[outer][x] = 1;
+            }
+
         }
-        for(int outer=0;outer< array.length;outer++){
-            System.out.printf("%d. line: %d%n",outer,array[outer].length);
+
+        for (int outer = 0; outer < array.length; outer++) {
+            System.out.printf("%d. line: %d%n", outer, array[outer].length);
         }
 
     }
